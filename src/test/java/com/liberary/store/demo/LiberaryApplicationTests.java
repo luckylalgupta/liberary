c2628fa4feedd62af6200dcd0e3bab1bbf2a0aba
Lucky Lal Gupta <luckylalgupta@gmail.com>
2021-02-12 14:16:11 +0530

    my first commit

---
+++ src/test/java/com/liberary/store/demo/LiberaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liberary.store.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
